2b37cdda42a09968884449dae36ac533fc754c7d
Jenna Reiners <51280601+J-M-R-01@users.noreply.github.com>
2022-08-22 03:40:29 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jenna Reiners
+ * Email: jreiners3@huskers.unl.edu
+ * Date: 2022/08/21
+ *
+ * basic hello world program in C
+ *
+ */
+
+int main(int argc, char **argv) {
+
+  printf("Jenna Reiners, Computer Science!\n");
+  printf("Major is Computer Science!\n");
+
+  return 0;
+}
